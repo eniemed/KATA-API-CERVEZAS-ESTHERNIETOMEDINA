1637b74e3015138dbc93b5a970165e9c5c1d3549
eniemed <115500235+eniemed@users.noreply.github.com>
2023-12-13 21:37:10 +0100

    Delete KATA-API-Esther_NM directory

--- KATA-API-Esther_NM/src/main/java/com/example/kataapiesther_nm/repos/StylesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.kataapiesther_nm.repos;
-
-import com.example.kataapiesther_nm.modelo.Styles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StylesRepo extends JpaRepository<Styles, Long> {
-}
